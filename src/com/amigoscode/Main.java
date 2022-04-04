4349104836b6946ec4ef7e2c2a3c074262f836e4
Minaf <minoo87>
2022-04-04 14:25:46 +0200

    Initial commit

---
+++ src/com/amigoscode/Main.java
@@ -0,0 +1,10 @@
+package com.amigoscode;
+
+import com.amigoscode.model.Product;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        
+    }
+}
